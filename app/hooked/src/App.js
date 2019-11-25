e57a320632ef0e34209c54a1eb7f130c29fa0856
bps_saito <bps_saito@bps-saitonoMacBook-puro.local>
2019-11-25 16:12:19 +0900

    入力値のHooksに保存及びボタン押下時の値取得処理_追加

--- app/hooked/src/App.js
+++ app/hooked/src/App.js
@@ -1,19 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-import {useState } from 'react';
 
 function App() {
-  const [movie_name, search_movie] = useState();
+  console.log("App init")
+  const [movie_name, set_MovieName] = useState("");
+
+  const handleChange = e => {
+    set_MovieName(e.target.value )
+  };
+
+  const printValues = e => {
+    e.preventDefault();
+    console.log("movie_name => " + movie_name);
+  };
+
   return (
     <div className="App">
       <h6>検索フォームの値「{movie_name}」</h6>
       <h1 className="hooked_title">HOOKED</h1>
-      {/* <form onSubmit={this.search_movie}> */}
-      <form onSubmit={() => search_movie(movie_name)}>
+      <form onSubmit={printValues} >
         <label>
           映画タイトル名：
-          <input className="search_field" type="text" value={movie_name}/>
+          <input className="search_field" type="text" value={movie_name} onChange={handleChange} />
         </label>
         <input className="search_btn" type="submit" value="SEARCH" />
       </form>
