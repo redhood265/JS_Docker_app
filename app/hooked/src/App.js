514cb28bee2581312992d41cfe1a51007c4c4359
bps_saito <bps_saito@bps-saitonoMacBook-puro.local>
2019-11-25 15:02:44 +0900

    実装中1

--- app/hooked/src/App.js
+++ app/hooked/src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {useState } from 'react';
 
 function App() {
+  const [movie_name, search_movie] = useState();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h6>検索フォームの値「{movie_name}」</h6>
+      <h1 className="hooked_title">HOOKED</h1>
+      {/* <form onSubmit={this.search_movie}> */}
+      <form onSubmit={() => search_movie(movie_name)}>
+        <label>
+          映画タイトル名：
+          <input className="search_field" type="text" value={movie_name}/>
+        </label>
+        <input className="search_btn" type="submit" value="SEARCH" />
+      </form>
     </div>
   );
 }
