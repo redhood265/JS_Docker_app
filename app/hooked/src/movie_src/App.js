4cb57c1ef5edf06f678fc20a77d006bd0f0204f6
bps_saito <bps_saito@bps-saitonoMacBook-puro.local>
2019-11-29 16:30:59 +0900

    チュートリアル写経途中

--- app/hooked/src/movie_src/App.js
+++ app/hooked/src/movie_src/App.js
@@ -8,9 +8,9 @@ import Search from "./Search";
 const MOVIE_API_URL = ""
 
 function App() {
-  const [loading, setLoading] = setState(true);
-  const [movies, setMovies] = setState([]);
-  const [erMsg, setErrorMessage] = setState(null);
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const [erMsg, setErrorMessage] = useState(null);
 
   useEffect(() => {
     fetch(MOVIE_API_URL).then(response => response.json).then(jsonResponse => {
@@ -19,35 +19,44 @@ function App() {
     });
   }, []);
     
-    const search = searchValue => {
-      setLoading(true);
-      setErrorMessage(null);
+  const search = searchValue => {
+    setLoading(true);
+    setErrorMessage(null);
 
-      fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=`)
-        .then(response => response.json())
-        .then(jsonResponse => {
-          if (jsonResponse.Response === "True") {
-            setMovies()
-            setLoading()
-          } else {
-            setErrorMessage()
-            setLoading()
-          }
-        })
-    }
+    fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=`)
+      .then(response => response.json())
+      .then(jsonResponse => {
+        if (jsonResponse.Response === "True") {
+          setMovies(jsonResponse.Search)
+          setLoading(false)
+        } else {
+          setErrorMessage(jsonResponse.Error)
+          setLoading(false)
+        }
+      });
+  };
+  
   return (
     <div classNmae="App">
       <Header Text="HOOKED_React" />
       <Search search={search} />
       <p className="App-intro">Sharing a few of our favourite movies</p>
+
       <div classNmae="movies">
-        {loading && !erMsg ?
-          (<span>loading...</span>) : erMsg ?
-            (<div className="errorMessage">{erMsg}</div>) : (movies.map((movie, index) => (<Movie key={`${index}-${movie.Title}`} movie={movie} />)))
+        {loading && !erMsg ? (
+          <span>loading...</span>
+        ) : erMsg ? (
+            <div className="errorMessage">{erMsg}</div>
+          ) : (
+              movies.map((movie, index) => (
+                <Movie key={`${index}-${movie.Title}`} movie={movie} />
+              )
+            ))
         }
       </div>
+
     </div>
   );
-}
+};
 
 export default App;
--- app/hooked/src/movie_src/Search.js
+++ app/hooked/src/movie_src/Search.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 const Search = (props) => {
 
-  const [searchValue, setSearchValue] = setState("");
+  const [searchValue, setSearchValue] = useState("");
 
   const handleSearchInputChanges = (e) => {
     setSearchValue(e.target.value)
